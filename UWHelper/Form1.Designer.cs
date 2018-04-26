ffd98174de144f6edd405f1de023c7be3e5c34f4
Adam Autio <adam.autio@gmail.com>
2018-04-26 13:59:02 +0200

    Initial Commit

---
+++ UWHelper/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace UWHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lstProcesses = new System.Windows.Forms.ListBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnBringToFront = new System.Windows.Forms.Button();
+            this.txtpY = new System.Windows.Forms.TextBox();
+            this.txtpX = new System.Windows.Forms.TextBox();
+            this.txtsX = new System.Windows.Forms.TextBox();
+            this.txtsY = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chkBorderless = new System.Windows.Forms.CheckBox();
+            this.tmrSetTopMost = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lstProcesses
+            // 
+            this.lstProcesses.FormattingEnabled = true;
+            this.lstProcesses.Location = new System.Drawing.Point(12, 12);
+            this.lstProcesses.Name = "lstProcesses";
+            this.lstProcesses.Size = new System.Drawing.Size(417, 264);
+            this.lstProcesses.TabIndex = 0;
+            this.lstProcesses.SelectedIndexChanged += new System.EventHandler(this.lstProcesses_SelectedIndexChanged);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(12, 282);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 8;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnBringToFront
+            // 
+            this.btnBringToFront.Location = new System.Drawing.Point(354, 366);
+            this.btnBringToFront.Name = "btnBringToFront";
+            this.btnBringToFront.Size = new System.Drawing.Size(75, 23);
+            this.btnBringToFront.TabIndex = 7;
+            this.btnBringToFront.Text = "Apply";
+            this.btnBringToFront.UseVisualStyleBackColor = true;
+            this.btnBringToFront.Click += new System.EventHandler(this.btnBringToFront_Click);
+            // 
+            // txtpY
+            // 
+            this.txtpY.Location = new System.Drawing.Point(354, 340);
+            this.txtpY.Name = "txtpY";
+            this.txtpY.Size = new System.Drawing.Size(75, 20);
+            this.txtpY.TabIndex = 5;
+            this.txtpY.Text = "0";
+            this.txtpY.TextChanged += new System.EventHandler(this.txtpY_TextChanged);
+            // 
+            // txtpX
+            // 
+            this.txtpX.Location = new System.Drawing.Point(354, 314);
+            this.txtpX.Name = "txtpX";
+            this.txtpX.Size = new System.Drawing.Size(75, 20);
+            this.txtpX.TabIndex = 3;
+            this.txtpX.Text = "0";
+            this.txtpX.TextChanged += new System.EventHandler(this.txtpX_TextChanged);
+            // 
+            // txtsX
+            // 
+            this.txtsX.Location = new System.Drawing.Point(232, 314);
+            this.txtsX.Name = "txtsX";
+            this.txtsX.Size = new System.Drawing.Size(75, 20);
+            this.txtsX.TabIndex = 1;
+            this.txtsX.Text = "1920";
+            this.txtsX.TextChanged += new System.EventHandler(this.txtsX_TextChanged);
+            // 
+            // txtsY
+            // 
+            this.txtsY.Location = new System.Drawing.Point(232, 340);
+            this.txtsY.Name = "txtsY";
+            this.txtsY.Size = new System.Drawing.Size(75, 20);
+            this.txtsY.TabIndex = 2;
+            this.txtsY.Text = "1080";
+            this.txtsY.TextChanged += new System.EventHandler(this.txtsY_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(313, 320);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "pos X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(313, 346);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "pos Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(191, 346);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "size Y";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(190, 320);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "size X";
+            // 
+            // chkBorderless
+            // 
+            this.chkBorderless.AutoSize = true;
+            this.chkBorderless.Location = new System.Drawing.Point(266, 371);
+            this.chkBorderless.Name = "chkBorderless";
+            this.chkBorderless.Size = new System.Drawing.Size(75, 17);
+            this.chkBorderless.TabIndex = 6;
+            this.chkBorderless.Text = "Borderless";
+            this.chkBorderless.UseVisualStyleBackColor = true;
+            // 
+            // tmrSetTopMost
+            // 
+            this.tmrSetTopMost.Tick += new System.EventHandler(this.tmrSetTopMost_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 401);
+            this.Controls.Add(this.chkBorderless);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtsY);
+            this.Controls.Add(this.txtsX);
+            this.Controls.Add(this.txtpX);
+            this.Controls.Add(this.txtpY);
+            this.Controls.Add(this.btnBringToFront);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.lstProcesses);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Autio Apps UW Helper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstProcesses;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnBringToFront;
+        private System.Windows.Forms.TextBox txtpY;
+        private System.Windows.Forms.TextBox txtpX;
+        private System.Windows.Forms.TextBox txtsX;
+        private System.Windows.Forms.TextBox txtsY;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox chkBorderless;
+        private System.Windows.Forms.Timer tmrSetTopMost;
+    }
+}
+
---
+++ UWHelper/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UWHelper
+{
+    public partial class Form1 : Form
+    {
+
+        /*
+         * Set up winAPI invokes
+         */
+        public static UIntPtr SetWindowLongPtr(IntPtr hWnd, Int32 nIndex, UIntPtr dwNewLong)
+        {
+            if (IntPtr.Size == 4)
+            {
+                return SetWindowLongPtr32(hWnd, nIndex, dwNewLong);
+            }
+            else
+            {
+                return SetWindowLongPtr64(hWnd, nIndex, dwNewLong);
+            }
+        }
+
+        [DllImport("user32.dll", SetLastError = true, EntryPoint = "SetWindowLong")]
+        private static extern UIntPtr SetWindowLongPtr32(IntPtr hWnd, Int32 nIndex, UIntPtr dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true, EntryPoint = "SetWindowLongPtr")]
+        private static extern UIntPtr SetWindowLongPtr64(IntPtr hWnd, Int32 nIndex, UIntPtr dwNewLong);
+
+        public static UIntPtr GetWindowLongPtr(IntPtr hWnd, Int32 nIndex)
+        {
+            if (IntPtr.Size == 4)
+            {
+                return GetWindowLongPtr32(hWnd, nIndex);
+            }
+            else
+            {
+                return GetWindowLongPtr64(hWnd, nIndex);
+            }
+        }
+
+        [DllImport("user32.dll", SetLastError = true, EntryPoint = "GetWindowLong")]
+        private static extern UIntPtr GetWindowLongPtr32(IntPtr hWnd, Int32 nIndex);
+
+        [DllImport("user32.dll", SetLastError = true, EntryPoint = "GetWindowLongPtr")]
+        private static extern UIntPtr GetWindowLongPtr64(IntPtr hWnd, Int32 nIndex);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+
+        // Set up constants
+        public static int GWL_EXSTYLE = -20;
+        public static int GWL_STYLE = -16;
+        public const uint WS_OVERLAPPED = 0x00000000;
+        public const uint WS_POPUP = 0x80000000;
+        public const uint WS_CHILD = 0x40000000;
+        public const uint WS_MINIMIZE = 0x20000000;
+        public const uint WS_VISIBLE = 0x10000000;
+        public const uint WS_DISABLED = 0x08000000;
+        public const uint WS_CLIPSIBLINGS = 0x04000000;
+        public const uint WS_CLIPCHILDREN = 0x02000000;
+        public const uint WS_MAXIMIZE = 0x01000000;
+        public const uint WS_CAPTION = 0x00C00000;     /* WS_BORDER | WS_DLGFRAME  */
+        public const uint WS_BORDER = 0x00800000;
+        public const uint WS_DLGFRAME = 0x00400000;
+        public const uint WS_VSCROLL = 0x00200000;
+        public const uint WS_HSCROLL = 0x00100000;
+        public const uint WS_SYSMENU = 0x00080000;
+        public const uint WS_THICKFRAME = 0x00040000;
+        public const uint WS_GROUP = 0x00020000;
+        public const uint WS_TABSTOP = 0x00010000;
+
+        public const uint WS_MINIMIZEBOX = 0x00020000;
+        public const uint WS_MAXIMIZEBOX = 0x00010000;
+        public const uint WS_EX_APPWINDOW = 0x00040000;
+        public const uint WS_TILED = WS_OVERLAPPED;
+        public const uint WS_ICONIC = WS_MINIMIZE;
+        public const uint WS_SIZEBOX = WS_THICKFRAME;
+        public const uint WS_TILEDWINDOW = WS_OVERLAPPEDWINDOW;
+
+        public const uint WS_OVERLAPPEDWINDOW =
+            (WS_OVERLAPPED |
+              WS_CAPTION |
+              WS_SYSMENU |
+              WS_THICKFRAME |
+              WS_MINIMIZEBOX |
+              WS_MAXIMIZEBOX);
+
+        public const uint WS_POPUPWINDOW =
+            (WS_POPUP |
+              WS_BORDER |
+              WS_SYSMENU);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        /*
+         * Start normal code
+         */
+
+        // Set up some global variables
+        public IntPtr handle;
+        public int sizeX, sizeY, posX, posY;
+        public bool topMost = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lstProcesses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        // handle integer texbox
+        string txtsXOld = "1920";
+        private void txtsX_TextChanged(object sender, EventArgs e)
+        {
+            long a;
+            if (txtsX.Text == "")
+            {
+                txtsX.Text = "0";
+            }
+            if (!long.TryParse(txtsX.Text, out a))
+            {
+                // If not int clear textbox text or Undo() last operation
+                txtsX.Text = txtsXOld;
+                txtsX.SelectionStart = txtsX.Text.Length;
+                txtsX.SelectionLength = 0;
+            }
+            txtsXOld = txtsX.Text;
+        }
+
+        string txtsYOld = "1080";
+        private void txtsY_TextChanged(object sender, EventArgs e)
+        {
+            long a;
+            if (txtsY.Text == "")
+            {
+                txtsY.Text = "0";
+            }
+            if (!long.TryParse(txtsY.Text, out a))
+            {
+                // If not int clear textbox text or Undo() last operation
+                txtsY.Text = txtsYOld;
+                txtsY.SelectionStart = txtsY.Text.Length;
+                txtsY.SelectionLength = 0;
+            }
+            txtsYOld = txtsY.Text;
+        }
+
+        string txtpXOld = "0";
+        private void txtpX_TextChanged(object sender, EventArgs e)
+        {
+            long a;
+            if (txtpX.Text == "")
+            {
+                txtpX.Text = "0";
+            }
+            if (!long.TryParse(txtpX.Text, out a))
+            {
+                // If not int clear textbox text or Undo() last operation
+                txtpX.Text = txtpXOld;
+                txtpX.SelectionStart = txtpX.Text.Length;
+                txtpX.SelectionLength = 0;
+            }
+            txtpXOld = txtpX.Text;
+        }
+
+        string txtpYOld = "0";
+        private void txtpY_TextChanged(object sender, EventArgs e)
+        {
+            long a;
+            if (txtpY.Text == "")
+            {
+                txtpY.Text = "0";
+            }
+            if (!long.TryParse(txtpY.Text, out a))
+            {
+                // If not int clear textbox text or Undo() last operation
+                txtpY.Text = txtpYOld;
+                txtpY.SelectionStart = txtpY.Text.Length;
+                txtpY.SelectionLength = 0;
+            }
+            txtpYOld = txtpY.Text;
+        }
+
+        // Refresh process list
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            lstProcesses.Items.Clear();
+            Process[] processes = Process.GetProcesses();
+            foreach (Process p in processes)
+            {
+                if (!String.IsNullOrEmpty(p.MainWindowTitle))
+                {
+                    lstProcesses.Items.Add(p.MainWindowTitle + " --- " + p.Id);
+                }
+            }
+        }
+
+        private void btnBringToFront_Click(object sender, EventArgs e)
+        {
+            if (lstProcesses.SelectedItem == null) { return; }
+            string SelectedItem = lstProcesses.GetItemText(lstProcesses.SelectedItem);
+            string toBeSearched = " --- ";
+            string pId = SelectedItem.Substring(SelectedItem.IndexOf(toBeSearched) + toBeSearched.Length);
+            int pIdInt = 0;
+            Int32.TryParse(pId, out pIdInt);
+            Process proc = Process.GetProcessById(pIdInt);
+            handle = proc.MainWindowHandle;
+            // convert textboxes to int
+            try
+            {
+                Int32.TryParse(txtpX.Text, out posX);
+                Int32.TryParse(txtpY.Text, out posY);
+                Int32.TryParse(txtsX.Text, out sizeX);
+                Int32.TryParse(txtsY.Text, out sizeY);
+            } catch
+            {
+                MessageBox.Show("Please only use numbers in size X, size Y, pos X and pos Y.");
+            }
+            
+            if (chkBorderless.Checked)
+            {
+
+                UIntPtr style = GetWindowLongPtr(handle, GWL_STYLE);
+                style = new UIntPtr(style.ToUInt64() & ~(UInt64)WS_OVERLAPPEDWINDOW);
+                style = new UIntPtr(style.ToUInt64() | (UInt64)WS_POPUPWINDOW);
+                SetWindowLongPtr(handle, GWL_STYLE, style);
+                SetWindowPos(handle, -2, posX, posY, sizeX, sizeY, 0x0010);
+                tmrSetTopMost.Enabled = true;
+            }
+            else
+            {
+                UIntPtr style = GetWindowLongPtr(handle, GWL_STYLE);
+                style = new UIntPtr(style.ToUInt64() & ~(UInt64)WS_POPUPWINDOW);
+                style = new UIntPtr(style.ToUInt64() | (UInt64)WS_OVERLAPPEDWINDOW);
+                SetWindowLongPtr(handle, GWL_STYLE, style);
+                SetWindowPos(handle, -2, posX, posY, sizeX, sizeY, 0x0010);
+                tmrSetTopMost.Enabled = false;
+            }
+        }
+
+        private void tmrSetTopMost_Tick(object sender, EventArgs e)
+        {
+            IntPtr fwHandle = GetForegroundWindow();
+            if (fwHandle == handle && fwHandle != null && handle != null && !topMost)
+            {
+                SetWindowPos(handle, -1, posX, posY, sizeX, sizeY, 0);
+                topMost = true;
+            } else if (fwHandle != handle && handle != null && topMost) {
+                SetWindowPos(handle, -2, posX, posY, sizeX, sizeY, 0x0010);
+                topMost = false;
+            }
+        }
+    }
+}
